34a072035c8e151467a5756c180bfcd6a3300668
Fabian Schulz <fabian.schulz2@hotmail.de>
2019-12-19 19:33:30 +0100

    GPIO Support eingebunden

--- Lichtorgel2.0/MainPage.xaml.cs
+++ Lichtorgel2.0/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using NAudio;
+using Unosquare.RaspberryIO;
+using Unosquare.WiringPi;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
@@ -22,9 +25,31 @@ namespace Lichtorgel2._0
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // NAudio.Wave.WaveFileReader wave = new NAudio.Wave.WaveFileReader("test.wav");
+            Pi.Init<BootstrapWiringPi>();    //doku : https://unosquare.github.io/raspberryio/
+            var blinkingPin = Pi.Gpio[0];
+            blinkingPin = Pi.Gpio[bcmPin:0];
+
+            blinkingPin.PinMode = Unosquare.RaspberryIO.Abstractions.GpioPinDriveMode.Output;
+            bool ison = false;
+            for(int i = 0; i < 20; i++)
+            {
+                ison = !ison;
+                blinkingPin.Write(ison);
+                System.Threading.Thread.Sleep(500);
+            }
+            
+
+
+            
         }
     }
 }
